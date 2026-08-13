9556a14bbe9692c7714ba4f9ce213054cebacc58
Deepak Verma <deepakverma050607@gmail.com>
2026-08-13 16:12:58 +0530

    Move all zeroes

---
+++ Arrays/moveAllzeroestoendofArray.py
@@ -0,0 +1,10 @@
+nums = [1,0,2,3,0,0,5,5,6,0,8,0,8]
+print(nums)
+n = len(nums)
+i=0
+for j in range(n):
+    if nums[j] != 0:
+        nums[i], nums[j] = nums[j], nums[i]
+        i+=1
+    j+=1
+print(nums)
