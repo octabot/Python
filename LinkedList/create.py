a2e1702b5a371b21180966a446c469d402a45bcc
Deepak Verma <deepakverma050607@gmail.com>
2026-08-17 17:58:20 +0530

    Small update in create.py

--- LinkedList/create.py
+++ LinkedList/create.py
@@ -9,10 +9,14 @@ print(a.data) #value = 5
 print(a.next)
 b = Node(7)
 print(b) #address of b
+print(a.next) #None bcz a.next is not assigned yet
 print(b.data) #value = 7
 print(b.next)
-a.next = b
+a.next = b # a.next is assigned
 print(a.data)
-print("\n\n")
+print(b)  #address of b
+print(a.next)  #address of b
+
+print("\n")
 head = a
 print(head.data)
